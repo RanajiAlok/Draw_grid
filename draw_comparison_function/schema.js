09ec84a49714168a7a460c7170c48d886d3b3bf4
Alok Kumar Singh <86158900+RanajiAlok@users.noreply.github.com>
2024-08-06 12:18:38 +0530

    Update schema.js

--- draw_comparison_function/schema.js
+++ draw_comparison_function/schema.js
@@ -1,61 +1,27 @@
 const schema = {
-    title: "Drawing Options",
-    type: "object",
-    properties: {
+  title: "Drawing Options",
+  type: "object",
+  properties: {
       left_image: {
-        type: "string",
-        enum: ["car", "coin", "star", "circle", "square", "polygon"],
-        title: "Left Image Type",
-        properties: {
-          polygon_sides: {
-            type: "integer",
-            title: "Number of sides",
-            minimum: 3,
-          },
-        },
+          type: "string",
+          enum: ["car", "coin", "star", "circle", "square", "triangle", "quadrilateral", "pentagon", "hexagon", "heptagon", "octagon", "nonagon", "decagon"],
+          title: "Left Image Type"
       },
       left_image_count: {
-        type: "number",
-        title: "Number of Cells for Left Image",
-        minimum: 1
-      },
-      left_rows: {
-        type: "number",
-        title: "Number of Rows for Left Grid",
-        minimum: 1
-      },
-      left_columns: {
-        type: "number",
-        title: "Number of Columns for Left Grid",
-        minimum: 1
+          type: "number",
+          title: "Number of Images for Left",
+          minimum: 1
       },
       right_image: {
-        type: "string",
-        enum: ["car", "coin", "star", "circle", "square", "polygon"],
-        title: "Right Image Type",
-        properties: {
-          polygon_sides: {
-            type: "integer",
-            title: "Number of sides",
-            minimum: 3,
-          },
-        },
+          type: "string",
+          enum: ["car", "coin", "star", "circle", "square", "triangle", "quadrilateral", "pentagon", "hexagon", "heptagon", "octagon", "nonagon", "decagon"],
+          title: "Right Image Type"
       },
       right_image_count: {
-        type: "number",
-        title: "Number of Cells for Right Image",
-        minimum: 1
-      },
-      right_rows: {
-        type: "number",
-        title: "Number of Rows for Right Grid",
-        minimum: 1
-      },
-      right_columns: {
-        type: "number",
-        title: "Number of Columns for Right Grid",
-        minimum: 1
-      },
-    },
-    required: ["left_image", "left_image_count", "left_rows", "left_columns", "right_image", "right_image_count", "right_rows", "right_columns"]
-  }; 
+          type: "number",
+          title: "Number of Images for Right",
+          minimum: 1
+      }
+  },
+  required: ["left_image", "left_image_count", "right_image", "right_image_count"]
+};
